755222e0102daa957726852910bb6928af914e4e
Cogi Asd <asadovserega2@gmail.com>
2024-07-26 23:46:12 +0300

    add new func

--- src/main.py
+++ src/main.py
@@ -7,12 +7,14 @@ def main():
     height = 100_000
 
     print(transfer(Earth,
-                   Earth['radius_meters'],
-                   Earth['radius_meters'] + height))
+                   6.5e6,
+                   13e6))
 
-    print(orbit_velocity(Earth, 6704490.9953, 6437215.67127))
+    print(orbit_velocity(Earth, 6.5e6, 13e6))
 
+    print(angle_transfer(6704490.9953, 6437215.67127) * RAD2DEG)
 
+    print(time_transfer(Earth, 6704490.9953, 6437215.67127))
 
 if __name__ == "__main__":
     main()
--- src/tools/astrodynamics.py
+++ src/tools/astrodynamics.py
@@ -1,8 +1,9 @@
 from src.constants import *
 from math import sqrt
 
-number  =   float | int
-data    =   dict[str, float]
+number = float | int
+data = dict[str, float]
+
 
 def transfer(body: dict,
              start_radius: number,
@@ -25,10 +26,6 @@ def transfer(body: dict,
             "total": dv0 + dv1}
 
 
-def c3(body: dict, vel_spacecraft: number) -> float:
-    return (0.5 * vel_spacecraft ** 2) - (body['mu'] / body['radius_meters'])
-
-
 def solve_ellipse_semi_major_axis(ap: number, pe: number) -> float:
     rmax, rmin = max(ap, pe), min(ap, pe)
     return (rmax + rmin) / 2
@@ -48,8 +45,10 @@ def energy(body: dict,
 def orbital_period(body: dict, ellipse_semi_major_axis: number) -> number:
     return TWO_PI * sqrt(ellipse_semi_major_axis ** 3 / body['mu']) / 1000
 
+
 def period(body: dict, semi_major_axis: number) -> number:
     return TWO_PI / sqrt(body['mu'] * semi_major_axis ** 1.5)
+
 
 def parabolic_velocity(body, height) -> float:
     return (2 * body["mu"] / (body['radius_meters'] + height)) ** 0.5
@@ -58,17 +57,37 @@ def parabolic_velocity(body, height) -> float:
 def escape_velocity(body: dict) -> float:
     return SQRT_2 * sqrt(body["g0"] * body["radius_meters"])
 
+
 def orbit_velocity(body: dict, radius_max: number, radius_min: number) -> data:
     MU_SQRT = sqrt(body['mu'])
     INV_SMA = 1 / solve_ellipse_semi_major_axis(radius_max, radius_min)
-    return {"ap_vel" : MU_SQRT * sqrt((2 / radius_max) - INV_SMA),
-            "pe_vel" : MU_SQRT * sqrt((2 / radius_min) - INV_SMA)}
+    return {"ap_vel": MU_SQRT * sqrt((2 / radius_max) - INV_SMA),
+            "pe_vel": MU_SQRT * sqrt((2 / radius_min) - INV_SMA)}
 
 
 def ecc(ap: number, pe: number) -> number:
     return (ap - pe) / (ap + pe)
 
 
-def angular_momentum(body: dict, ap:number, ep: number) -> number:
+def type_orbit(energy_spacecraft: number) -> str:
+    if energy_spacecraft < 0:
+        return "elliptical orbit"
+
+    elif energy_spacecraft == 0:
+        return "parabolic orbit"
+
+    elif energy_spacecraft > 0:
+        return "hyperbolic orbit"
+
+
+def angular_momentum(body: dict, ap: number, ep: number) -> number:
     semimajor_axis = solve_ellipse_semi_major_axis(ap, ep)
-    return sqrt(body['mu'] * semimajor_axis * (1 - ecc(ap, ep)**2))
+    return sqrt(body['mu'] * semimajor_axis * (1 - ecc(ap, ep) ** 2))
+
+
+def time_transfer(body: dict, start_radius: number, end_radius: number) -> float:
+    return PI * sqrt((start_radius + end_radius) ** 3 / 8 * body['mu'])
+
+
+def angle_transfer(start_radius: number, end_radius: number) -> float:
+    return PI * (1 - 0.353553391 * sqrt((start_radius / end_radius + 1) ** 3))
